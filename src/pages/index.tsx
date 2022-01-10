5dc684790fe0b198eba24062057d31f371bd394b
Daniel da Costa Bezerra <daniel.c.dan@gmail.com>
2022-01-10 13:32:44 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import Botao from "../components/Botao"
+import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
+
+export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Zart', 42, '2'),
+    new Cliente('Danny', 35, '3'),
+    new Cliente('Cecília', 39, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir...${cliente.nome}`)
+  }
+
+
+  return (
+    <div className={`flex justify-center items-center h-screen bg-gradient-to-r
+     from-blue-500 to-purple-500 text-white`}>
+     <Layout titulo="Cadastro Simples">
+       <div className="flex justify-end">
+        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+       </div>
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+     </Layout>
+    </div>
+  )
+}
